62f95feef1fabaac75fc11e06731a19692566f53
codevicium <ezequiel.camussi@flexxus.com>
2020-06-20 15:43:09 -0300

    clase 3

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-window.onload = function () {
-  let carusel = this.document.getElementById("carousel");
-  this.console.log(carusel);
-};
+var img = [];
+function createNuevoField(){
+  let input = document.createElement("input");
+  input.classList.add("form-control");
+  let carusel = document.getElementById("carousel");
+  let tag = carusel.getElementsByTagName("input");
+  console.log(tag,"tag")
+  carusel.appendChild(input);
+}
+
+
