b59dc6211fadd9d36a771732221b7c2908931110
codevicium <ezequiel.camussi@flexxus.com>
2020-07-04 15:11:35 -0300

    clase 04/07/20

--- mojjitoV1/javascript.js
+++ mojjitoV1/javascript.js
@@ -17,8 +17,13 @@ function createNuevoField() {
 function esPar(number) {
   return (number % 2) === 0;
 }
+// que el cliente vea el precio con el valor de la moneda local 
 function agregarProducto(){
-  let nombre = document.getElementById("name").value;
+  // todos tienen que tener un valor
+  // nombre y descripcion tiene que estar el dato que vas a guardar todo en minuscula 
+  // descripcion del producto no puede superar los 140 caracteres
+  // ante cada interaccion que tengamos con el usuario muestro un cartel que le indique cual fue su error 
+  let nombre = document.getElementById("name").value; 
   let descripcion = document.getElementById("description").value;
   let precio = document.getElementById("price").value;
   let img = document.getElementById("image").value;
@@ -28,8 +33,9 @@ function agregarProducto(){
 }
 
 function listarProductos() {
+  // nombre descripcion se tienen que mostrar la primera letra en mayuscula
+  // tiene que estar en la moneda local $
   let tbody = document.getElementById("tbody");
-  console.log(tbody);
   for (let index = 0; index < productos.length; index++) {
     let img = document.createElement("img");
     let tr = tbody.insertRow();
@@ -38,10 +44,8 @@ function listarProductos() {
     tr.insertCell().innerText= productos[index].nombre;
     tr.insertCell().innerText= productos[index].descripcion;
     tr.insertCell().innerText= productos[index].precio;
-   
   }
  }
 window.onload = () => {
   listarProductos()
-  console.log(table,"table");
 };
