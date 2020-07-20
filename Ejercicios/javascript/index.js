d5893c65d88952072a2df35ba4524d0588bb96df
jonathanvicium <jonathanoscarloez@outlook.com>
2020-07-19 21:56:18 -0300

    trabajndo con alet y parte de variables

--- Ejercicios/javascript/index.js
+++ Ejercicios/javascript/index.js
@@ -5,3 +5,20 @@ let mostrarSalames = () => {
     console.log(`${salames[i][0].toUpperCase()}${salames[i].slice(1)}`);
   }
 };
+
+var saludo = " hola ",nombre,final = " !",resultado;
+{
+  nombre = prompt("soy ATLAS cual es tu nombre");
+  resultado = saludo + nombre + final;
+  alert(resultado);
+}
+
+let pregunta = "! ",accion,signo = " buenisimo a buscarlo!",resultado1;
+{
+  accion = prompt("que te gustaria saber hoy de bebidas?");
+  resultado1 = pregunta + accion + signo;
+  alert(resultado1);
+}
+
+let sugerir = "entra a nuestra pagina y encuentra lo que buscas tenemos mucho de eso gracias!";
+alert(sugerir);
