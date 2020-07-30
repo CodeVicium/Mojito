76f814c67fe40d87e519debfdf9ebe64facc69e6
jonathanvicium <jonathanoscarloez@outlook.com>
2020-07-30 20:32:11 -0300

    tratando de hacer la validacion de datos

--- Ejercicios/javascript/index.js
+++ Ejercicios/javascript/index.js
@@ -1,42 +1,50 @@
-let error = document.getElementById("error");
-Error.style.color="red";
+//let error = document.getElementById("error");
 
 let jugar = () => {
-  // el nombre de los jugadore solo se pide una vez despues no se deben mostrar 
-  // se deberia mostrar despues de la primera vez solo un cuadro con su nombre 
-  console.log(document.getElementsByName("name"));
-  let player1 = document.getElementsByName("name")[0].value;
-  let player2 = document.getElementsByName("name")[1].value;
-  let mensajeserror = [];
+  // el nombre de los jugadore solo se pide una vez despues no se deben mostrar
+  // se deberia mostrar despues de la primera vez solo un cuadro con su nombre
+  console.log(document.getElementsByName("jugador"));
+  let player1 = document.getElementsByName("jugador")[0].value;
+  let player2 = document.getElementsByName("jugador")[1].value;
+  let mensajeserror =[];
 
-  if(name.value === null || name.value === ""){
-    mensajeserror.push("ingresa un nombre")
-  }
-  error.innerHTML = mensajeserror.join(", ");
-
+  
   // validar que el usuario haya escrito un valor
   let valorPlayer1 = Math.ceil(Math.random() * 6);
   let valorPlayer2 = Math.ceil(Math.random() * 6);
   let paternPlayer1 = document.getElementById("player1");
   let paternPlayer2 = document.getElementById("player2");
-  // validar que no hay imagenes anteriores antes de poner una nueva 
-  // poner loading mientras se define el resultado y el resultado debe tardar 4 segundos 
+
+
+  // validar que no hay imagenes anteriores antes de poner una nueva
+  // poner loading mientras se define el resultado y el resultado debe tardar 4 segundos
+
+
+
   let imgPlayer1 = document.createElement("img");
-  imgPlayer1.setAttribute("src",`./img/${valorPlayer1}.jpg`)
-  let imgPlayer2= document.createElement("img");
-  imgPlayer2.setAttribute("src",`./img/${valorPlayer2}.jpg`)
+  imgPlayer1.setAttribute("src", `./img/${valorPlayer1}.jpg`);
+  let imgPlayer2 = document.createElement("img");
+  imgPlayer2.setAttribute("src", `./img/${valorPlayer2}.jpg`);
+
   
   paternPlayer1.appendChild(imgPlayer1);
   paternPlayer2.appendChild(imgPlayer2);
+
+
   let ganador =
     valorPlayer1 > valorPlayer2
-      ? `gano ${player1}`
-      : valorPlayer1 === valorPlayer2
+    ? `gano ${player1}`
+    : valorPlayer1 === valorPlayer2
       ? ` Fue empate`
       : `gano ${player2}`;
-    
-  alert(`El resultado fue ${ganador}`); // el resultado se debe mostrar bonito al costado derecho de las imagenes 
 
-  
-  
-};
+      
+      //let mensajeserror = [0,1];
+
+
+      //if (jugador1.value === null || jugador1.value === " ") {
+        //mensajeserror.push("ingresa un nombre");
+      //}
+      
+      alert(`El ganador fue ${ganador}`); // el resultado se debe mostrar bonito al costado derecho de las imagenes
+    }
