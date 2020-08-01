9f2060fc10158347e86b86b8e4c4ac58cf2b2d8b
CodeVicium <byboygringo@gmail.com>
2020-08-01 15:58:19 -0300

    clase 1/08/2020

--- Ejercicios/javascript/index.js
+++ Ejercicios/javascript/index.js
@@ -1,45 +1,79 @@
-//let error = document.getElementById("error");
-
+let saveValues;
 let jugar = () => {
   // el nombre de los jugadore solo se pide una vez despues no se deben mostrar
   // se deberia mostrar despues de la primera vez solo un cuadro con su nombre
   console.log(document.getElementsByName("jugador"));
-  let player1 = document.getElementsByName("jugador")[0].value;
-  let player2 = document.getElementsByName("jugador")[1].value;
+  if (!saveValues) {
+    let player1 = document.getElementsByName("jugador")[0].value;
+    let player2 = document.getElementsByName("jugador")[1].value;
+    if (player1 && player2) {
+      saveValues = [player1, player2];
+    } else {
+      alert("no ingreso correctamente los nombres");
+      return;
+    }
+  }
+
   // validacion ligica que obtiene como resultado un booleano
   /* falsy values :
      variable = undefined,null,false,0,"";
      validacion por ancho de array : variable.legth va a dar falso cuando el array este vacio
   */
-  if (player1 && player2) {
-    // validar que el usuario haya escrito un valor
-    let valorPlayer1 = Math.ceil(Math.random() * 6);
-    let valorPlayer2 = Math.ceil(Math.random() * 6);
-    let paternPlayer1 = document.getElementById("player1");
-    let paternPlayer2 = document.getElementById("player2");
 
-    // validar que no hay imagenes anteriores antes de poner una nueva
-    // poner loading mientras se define el resultado y el resultado debe tardar 4 segundos
+  let valorPlayer1 = Math.ceil(Math.random() * 6);
+  let valorPlayer2 = Math.ceil(Math.random() * 6);
+  let paternPlayer1 = document.getElementById("player1");
+  let paternPlayer2 = document.getElementById("player2");
 
-    let imgPlayer1 = document.createElement("img");
-    imgPlayer1.setAttribute("src", `./img/${valorPlayer1}.jpg`);
-    let imgPlayer2 = document.createElement("img");
-    imgPlayer2.setAttribute("src", `./img/${valorPlayer2}.jpg`);
+  // validar que no hay imagenes anteriores antes de poner una nueva
+  // poner loading mientras se define el resultado y el resultado debe tardar 4 segundos
 
-    paternPlayer1.appendChild(imgPlayer1);
-    paternPlayer2.appendChild(imgPlayer2);
+  console.log(paternPlayer1.children, "papa1");
+  console.log(paternPlayer2.children, "papa2");
+  const validarSiTieneUnHIjoImg = (arrayDeHijos) => {
+    console.log(arrayDeHijos, "array de hijos");
+    let tuboHijoImg = false;
 
-    let ganador =
-      valorPlayer1 > valorPlayer2
-        ? `gano ${player1}`
-        : valorPlayer1 === valorPlayer2
-        ? ` Fue empate`
-        : `gano ${player2}`;
-
-    alert(`El ganador fue ${ganador}`); // el resultado se debe mostrar bonito al costado derecho de las imagenes
+    for (let i = 0; i < arrayDeHijos.length; i++) {
+      console.log(arrayDeHijos[i]);
+    }
+    return tuboHijoImg;
+  };
+  if (
+    validarSiTieneUnHIjoImg(paternPlayer1.children) &&
+    validarSiTieneUnHIjoImg(paternPlayer2.children)
+  ) {
+    console.log("paso");
   } else {
-    alert(
-      "debera cargar los nombres de los jugadores antes de iniciar el juego"
-    );
+    console.log("no paso");
   }
+
+  let imgPlayer1 = document.createElement("img");
+  imgPlayer1.setAttribute("src", `./img/${valorPlayer1}.jpg`);
+  imgPlayer1.style.maxWidth = "8rem";
+  /*
+    para agregar un clase de css a un nodo de tipo html
+    imgPlayer1.classList.add("pepe");
+    
+    para eliminar un clase de css a un nodo de tipo html
+    imgPlayer1.classList.remove("pepe");
+    */
+
+  let imgPlayer2 = document.createElement("img");
+  imgPlayer2.setAttribute("src", `./img/${valorPlayer2}.jpg`);
+  imgPlayer2.style.maxWidth = "8rem";
+
+  paternPlayer1.appendChild(imgPlayer1);
+  paternPlayer2.paternPlayer2.appendChild(imgPlayer2);
+
+  let ganador =
+    valorPlayer1 > valorPlayer2
+      ? `gano ${player1}`
+      : valorPlayer1 === valorPlayer2
+      ? ` Fue empate`
+      : `gano ${player2}`;
+
+  alert(`El ganador fue ${ganador}`); // el resultado se debe mostrar bonito al costado derecho de las imagenes
+
+  alert("debera cargar los nombres de los jugadores antes de iniciar el juego");
 };
